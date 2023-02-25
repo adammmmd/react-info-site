8f55fa2c59d56d418efa9772827482aa95548481
adammmmd <adam.muhammad.onduty@gmail.com>
2023-02-25 14:27:55 +0700

    adding existing component files

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Nav(props) {
+    return (
+        <nav className={props.lightMode ? "light" : ""}>
+            <div className="title">
+                <img src="./src/assets/react-icon-small.png" className="logo-nav" />
+                <h3 className="title-nav">ReactFacts</h3>
+            </div>
+            <div className="toggler">
+                <p className="toggler-light">Dark</p>
+                <div className="toggler-slider" onClick={props.toggleDarkMode}>
+                    <div className="toggler-slider-circle"></div>
+                </div>
+                <p className="toggler-dark">Light</p>
+            </div>
+        </nav>
+    )
+}
